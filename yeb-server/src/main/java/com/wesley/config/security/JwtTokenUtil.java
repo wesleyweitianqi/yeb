08e89c251003e44d444f9640ccb3afff66e9cafa
wesleyweitianqi <wesleytianqiwei@gmail.com>
2023-11-05 22:56:52 -0500

    implement jwt

---
+++ yeb-server/src/main/java/com/wesley/config/security/JwtTokenUtil.java
@@ -0,0 +1,84 @@
+package com.wesley.config.security;
+
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtTokenUtil {
+    private static final String CLAIM_KEY_USERNAME="sub";
+    private static final String CLAIM_KEY_CREATED="created";
+
+    @Value("${jwt.secret}")
+    private  String secret;
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+
+    //generate token by user info
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put(CLAIM_KEY_USERNAME, userDetails.getUsername());
+        claims.put(CLAIM_KEY_CREATED, new Date());
+        return generateToken(claims);
+    }
+    private String generateToken(Map<String, Object> claims) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setExpiration(generateExpirationDate())
+                .signWith(SignatureAlgorithm.ES512, secret)
+                .compact();
+    }
+
+    private Date generateExpirationDate() {
+        return new Date(System.currentTimeMillis() + expiration * 1000);
+    }
+
+    public String getUsernameFromToken(String token) {
+        String username;
+        try{
+            Claims claims = getClaimsFromToken(token);
+            username = claims.getSubject();
+        }catch (Exception e){
+            username = null;
+        }
+        return username;
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails){
+        String username = getUsernameFromToken(token);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        Claims claims = getClaimsFromToken(token);
+        Date expiration = claims.getExpiration();
+        return expiration.before(new Date());
+    }
+
+    private boolean canRefresh(String token) {
+        return !isTokenExpired(token);
+    }
+
+    public String refreshToken(String token){
+        Claims claims = getClaimsFromToken(token);
+        claims.put(CLAIM_KEY_CREATED, new Date());
+        return generateToken(claims);
+    }
+
+    private Claims getClaimsFromToken(String token) {
+        Claims claims = null;
+        try {
+            claims = Jwts.parser()
+                    .setSigningKey(secret)
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return claims;
+    }
+}
