2f4cc81da9a32f74d8ab60bcec5cd69a8978c5fa
wesleyweitianqi <wesleytianqiwei@gmail.com>
2023-11-18 11:31:24 -0500

    GlobalException config

---
+++ yeb-server/src/main/java/com/wesley/config/exception/GlobalException.java
@@ -0,0 +1,20 @@
+package com.wesley.config.exception;
+
+
+import com.wesley.pojo.RespBean;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+
+@RestControllerAdvice
+public class GlobalException {
+    @ExceptionHandler(SQLException.class)
+    public RespBean sqlException(SQLException e){
+        if (e instanceof SQLIntegrityConstraintViolationException){
+            return RespBean.error("Operation failed due to data-related issues!");
+        }
+        return RespBean.error("database exception");
+    }
+}
